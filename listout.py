07068789041477fb56fe39d8a27ea1ca9a1c829e
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2020-12-16 18:45:24 +0900

    Create listout.py
    [下位語、上位語]の形で出力するようにしました。

---
+++ listout.py
@@ -0,0 +1,21 @@
+import wn
+
+def abstract_word(lemma):
+    result = []
+    for word in wn.getWords(lemma):
+        for sense in wn.getSenses(word):
+            if sense.src != 'hand': continue
+            for synlink in wn.getSynLinks(sense, 'hype'):
+                abst_sense = wn.getSense(synlink.synset2)
+                if abst_sense and word.wordid != abst_sense.wordid:
+                    result.append(wn.getWord(abst_sense.wordid).lemma)
+    return result
+
+words = wn.getWordsByLang()
+
+for word in words:
+    lemma = word.lemma
+    # print(lemma)
+
+    for abst_word in abstract_word(lemma=lemma):
+        print([lemma,abst_word]) #[下位語、上位語]の形で出力
