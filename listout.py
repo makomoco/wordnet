dee8b60a6e2ea4a277a55a0bb65f80ed17118a87
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2020-12-21 00:36:44 +0900

    リストとタプルの位置関係が逆になっていたので修正

--- listout.py
+++ listout.py
@@ -1,15 +1,15 @@
 import wn
 
 def abstract_word(lemma):
-    result = []
+    result1 = []
     for word in wn.getWords(lemma):
         for sense in wn.getSenses(word):
             if sense.src != 'hand': continue
             for synlink in wn.getSynLinks(sense, 'hype'):
                 abst_sense = wn.getSense(synlink.synset2)
                 if abst_sense and word.wordid != abst_sense.wordid:
-                    result.append(wn.getWord(abst_sense.wordid).lemma)
-    return result
+                    result1.append(wn.getWord(abst_sense.wordid).lemma)
+    return result1
 
 words = wn.getWordsByLang()
 
@@ -20,12 +20,12 @@ def listout():
         # print(lemma)
 
         for abst_word in abstract_word(lemma=lemma):
-            result2.append([lemma,abst_word])
-            # print([lemma,abst_word]) #[下位語、上位語]の形で出力
+            result2.append((lemma,abst_word))
+            # print([lemma,abst_word]) #(下位語、上位語)のタプル形式で出力
 
     return result2
 
 with open('WordNet_list.txt', 'w') as f:
-    print(tuple(listout()), file=f)
+    print(listout(), file=f)
 
 # print(listout()) # 日本語WordNetの下位上位概念を全て出力
