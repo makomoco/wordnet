e5c93d8c80df4fed2a86fd7f9d2346cbe270b535
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-10 17:36:25 +0900

    Added mecab file

--- mecab_sample.py
+++ mecab_sample.py
@@ -4,10 +4,12 @@ import MeCab
 # MeCab::Taggerクラスのインスタンスを作成（ここではデフォルト設定）
 m = MeCab.Tagger('-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd')
 
-sentence = '鬼滅の刃の映画を見に行った。'
+#sentence = '鬼滅の刃の映画を見に行った。'
+with open("sentence.txt") as f:
+    text = f.read()
 
 # 日本語文章の解析処理
-ma = m.parse(sentence)
+ma = m.parse(text)
 
 # 解析結果を表示
 print(ma)
