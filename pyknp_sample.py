40b655d1886b14ce83e4cdd43e8aa260f47e92c0
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-11 19:09:54 +0900

    Update pyknp_sample.py

--- pyknp_sample.py
+++ pyknp_sample.py
@@ -2,14 +2,19 @@
 from __future__ import unicode_literals # It is not necessary when you use python3.
 from pyknp import Juman
 
+result3 = []
+
 with open("sentence.txt") as f:
     text = f.read()
 
 juman = Juman(jumanpp=False)   # default is JUMAN++: Juman(jumanpp=True). if you use JUMAN, use Juman(jumanpp=False)
 result = juman.analysis(text)
+
 for mrph in result.mrph_list(): # 各形態素にアクセス
     #print("見出し:%s, 読み:%s, 原形:%s, 品詞:%s, 品詞細分類:%s, 活用型:%s, 活用形:%s, 意味情報:%s, 代表表記:%s" \
             #% (mrph.midasi, mrph.yomi, mrph.genkei, mrph.hinsi, mrph.bunrui, mrph.katuyou1, mrph.katuyou2, mrph.imis, mrph.repname))
 
-    if(mrph.hinsi == "名詞"):
-        print("%s" % (mrph.midasi))
+    if(mrph.hinsi == "名詞"): #名詞のみ抽出
+        result3.append('%s' % (mrph.midasi))
+
+print(result3)
