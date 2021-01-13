7b315c4f25aa94c0ad91cd440bef7eb29fd653b7
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-13 12:07:32 +0900

    Update mecab01.py

--- mecab01.py
+++ mecab01.py
@@ -2,4 +2,4 @@ from parser import MecabParser
 
 mp =  MecabParser(word_classes=['名詞'], word_class_details=['一般','固有名詞'])
 text = '今日はお腹が空いたので近所の天下一品のラーメンを食べに来ました。'
-mp.parse(text)
+print(mp.parse(text))
