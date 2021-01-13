cce479a425721b974e5bcc92a051e732bd51e63c
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-13 17:42:46 +0900

    Update mecab01.py

--- mecab01.py
+++ mecab01.py
@@ -1,11 +1,11 @@
 import premecab
 
 # 要求仕様書１
-	termlist1 = []
-	newlist1 = []
-	file1 = 'sentence_after.txt'
-	new_file1 = 'sentence_after.txt'
-	analysis_1(termlist1,new_file1)
-	#analysis_2(termlist1,new_file1)
+termlist1 = []
+#newlist1 = []
+#file1 = 'sentence_after.txt'
+new_file1 = 'sentence_after.txt'
+analysis_1(termlist1,new_file1)
+#analysis_2(termlist1,new_file1)
 
 print(termlist1)
