2ed3ae4eb352af80138c7d3ae733004b666e172e
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-17 19:28:34 +0900

    Update mecab01.py

--- mecab01.py
+++ mecab01.py
@@ -2,10 +2,16 @@ from premecab import analysis_1, analysis_2
 
 # 要求仕様書１
 termlist1 = []
+termlist2 = []
 #newlist1 = []
 #file1 = 'sentence_after.txt'
 new_file1 = 'sentence_after.txt'
-#analysis_1(termlist1,new_file1)
-analysis_2(termlist1,new_file1)
+print(1.名詞⁺を抽出)
+analysis_1(termlist1,new_file1)
 
 print([t.word for t in termlist1])
+
+print(2.（名詞⁺）＋（を、に、から、へ）＋（サ変名詞OR動詞）を抽出)
+analysis_2(termlist2,new_file1)
+
+print([t.word for t in termlist2])
