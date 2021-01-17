2394b71b12a6ee818561bb2c1022ec7c8ae542b7
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-17 19:35:58 +0900

    Update mecab01.py

--- mecab01.py
+++ mecab01.py
@@ -11,7 +11,7 @@ analysis_1(termlist1,new_file1)
 
 print([t.word for t in termlist1])
 
-print(名詞+）＋（を、に、から、へ）＋（サ変名詞OR動詞）を抽出)
+print((名詞+）＋（を、に、から、へ）＋（サ変名詞OR動詞）を抽出)
 analysis_2(termlist2,new_file1)
 
 print([t.word for t in termlist2])
