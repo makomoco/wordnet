2a8bfa79dce4b2f45fd67f434c2b7ab954c72cce
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-17 19:41:41 +0900

    Update mecab01.py

--- mecab01.py
+++ mecab01.py
@@ -6,12 +6,13 @@ termlist2 = []
 #newlist1 = []
 #file1 = 'sentence_after.txt'
 new_file1 = 'sentence_after.txt'
-print('名詞+を抽出')
+print('パターン1:名詞+を抽出')
 analysis_1(termlist1,new_file1)
 
 print([t.word for t in termlist1])
+print(¥n¥n¥n)
 
-print(('(名詞+)+(を、に、から、へ)+(サ変名詞OR動詞)を抽出'))
+print(('パターン2:(名詞+)+(を、に、から、へ)+(サ変名詞OR動詞)を抽出'))
 analysis_2(termlist2,new_file1)
 
 print([t.word for t in termlist2])
