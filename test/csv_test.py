b571aacae9691ee47453d2b7d4c889db16e45918
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-20 22:13:24 +0900

    Update csv_test.py

--- test/csv_test.py
+++ test/csv_test.py
@@ -1,11 +1,16 @@
 import csv
 import numpy as np
 
+# with open("aiueo.csv") as fp:
+#     reader = csv.reader(fp)
+#     data = [ e for e in reader ]
+
+# data = np.array(data).reshape(-1)
+
+# #以下確認のための出力
+# print(data)
+
 with open("aiueo.csv") as fp:
-    reader = csv.reader(fp)
-    data = [ e for e in reader ]
-
-data = np.array(data).reshape(-1)
-
-#以下確認のための出力
-print(data)
+    csvList = list(csv.reader(fp))
+flatList = [item for subList in csvList for item in subList]
+print(flatList)
