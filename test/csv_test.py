0cfe26a4524c4905f812da5a197ce1a437e62238
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-20 22:15:16 +0900

    Update csv_test.py

--- test/csv_test.py
+++ test/csv_test.py
@@ -1,16 +1,13 @@
 import csv
 import numpy as np
 
-# with open("aiueo.csv") as fp:
-#     reader = csv.reader(fp)
-#     data = [ e for e in reader ]
-
-# data = np.array(data).reshape(-1)
-
-# #以下確認のための出力
-# print(data)
-
 with open("aiueo.csv") as fp:
     csvList = list(csv.reader(fp))
 flatList = [item for subList in csvList for item in subList]
 print(flatList)
+
+list_2 = ['あ','は']
+
+ls = [l for l in list_2 if l in flatList] 
+
+print(ls)
