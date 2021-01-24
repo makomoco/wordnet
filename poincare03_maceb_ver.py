c2fefd15cf8e02908adfc41a28a5c39742ae413d
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-24 17:20:10 +0900

    Update poincare03_maceb_ver.py

--- poincare03_maceb_ver.py
+++ poincare03_maceb_ver.py
@@ -231,9 +231,9 @@ print('ソートした結果を表示')
 distance_and_word_list_sorted = sorted(distance_and_word_list_hukugougo)
 print(sorted(distance_and_word_list_hukugougo))
 
-print('キッチン' in list_from_WordNet_2)
-print('キッチン' in ls)
-print('キッチン' in l3_l2_and_list)
+print('給湯' in list_from_WordNet_2)
+print('給湯' in ls)
+print('給湯' in l3_l2_and_list)
 
 ######################
 # 指定した単語の距離を出力
