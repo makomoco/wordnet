d158e575308c2c314a60061974b4d212198952e3
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-24 15:36:10 +0900

    a

--- poincare03_maceb_ver.py
+++ poincare03_maceb_ver.py
@@ -86,7 +86,20 @@ list_from_POL = ['単位', '時間', '要求', 'n', '秒', '一', 'dE', 'ため'
 
 ls = [l for l in list_from_POL if l in list_from_WordNet_2] 
 
-print('ポット' in list_from_WordNet_2)
+# 複合語の平均ベクトルを求める
+aa_1 = model.kv['状態']
+aa_2 = model.kv['表示']
+
+bb_1 = aa_1.tolist()
+bb_2 = aa_2.tolist()
+
+avg_x = (bb_1[0] + bb_2[0]) / 2
+avg_y = (bb_1[1] + bb_2[1]) / 2
+
+avg = [avg_x,avg_y]
+
+print('平均ベクトルを算出')
+print(avg)
 
 # aaa = model.kv['ポット']
 # bbb = aaa.tolist()
