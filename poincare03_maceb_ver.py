e07d1d7b397a2afa2b3715690636fa846e73a52b
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-24 14:32:16 +0900

    複合語処理

--- poincare03_maceb_ver.py
+++ poincare03_maceb_ver.py
@@ -86,7 +86,8 @@ list_from_POL = ['単位', '時間', '要求', 'n', '秒', '一', 'dE', 'ため'
 
 ls = [l for l in list_from_POL if l in list_from_WordNet_2] 
 
-print(model.kv['ポッド'])
+str_1 = input()
+print(model.kv['%s'] % str_1)
 
 # WordNet内に登録されている単語のみ出力
 # print('話題沸騰ポッド内の単語でWordNet内に登録されている単語のみ出力')
