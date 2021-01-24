e6a486ea572478a80af54fc91bde61fa8861c7b3
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-24 14:57:53 +0900

    a

--- poincare03_maceb_ver.py
+++ poincare03_maceb_ver.py
@@ -86,10 +86,10 @@ list_from_POL = ['単位', '時間', '要求', 'n', '秒', '一', 'dE', 'ため'
 
 ls = [l for l in list_from_POL if l in list_from_WordNet_2] 
 
-print(model.kv['状態'])
-print(model.kv['表示'])
-print(model.kv['ロック'])
-print(model.kv['ランプ'])
+aaa = model.kv['温度']
+bbb = aaa.tolist()
+print(bbb)
+
 
 # WordNet内に登録されている単語のみ出力
 # print('話題沸騰ポッド内の単語でWordNet内に登録されている単語のみ出力')
