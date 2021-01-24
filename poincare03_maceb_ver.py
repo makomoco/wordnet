492939d32c30e914098e382439f2ff438ffcb31c
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-24 15:51:54 +0900

    Update poincare03_maceb_ver.py

--- poincare03_maceb_ver.py
+++ poincare03_maceb_ver.py
@@ -87,7 +87,7 @@ list_from_POL = ['単位', '時間', '要求', 'n', '秒', '一', 'dE', 'ため'
 ls = [l for l in list_from_POL if l in list_from_WordNet_2] 
 
 # 複合語の平均ベクトルを求める
-aa_1 = model.kv['高温']
+aa_1 = model.kv['節約']
 aa_2 = model.kv['モード']
 
 bb_1 = aa_1.tolist()
