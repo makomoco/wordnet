54785f0aaacb1d4e84a9e1ba5a9fc291ce61d4b2
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-24 15:22:27 +0900

    a

--- poincare03_maceb_ver.py
+++ poincare03_maceb_ver.py
@@ -86,6 +86,9 @@ list_from_POL = ['単位', '時間', '要求', 'n', '秒', '一', 'dE', 'ため'
 
 ls = [l for l in list_from_POL if l in list_from_WordNet_2] 
 
+aaa = model.kv['ポッド']
+bbb = aaa.tolist()
+print(bbb)
 
 # WordNet内に登録されている単語のみ出力
 # print('話題沸騰ポッド内の単語でWordNet内に登録されている単語のみ出力')
