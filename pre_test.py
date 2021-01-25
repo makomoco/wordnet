02b8f676f9c089f72b1be86baa72f13e5c23eea4
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-25 16:38:41 +0900

    add pre_test.py

---
+++ pre_test.py
@@ -0,0 +1,25 @@
+import neologdn
+import re
+
+text = '今日は、とっっってもおいしーーーいモンブランを２つも食べました。1個５00円もしました。'
+
+#全角・半角の統一と重ね表現の除去 (neologdn)
+normalized_text = neologdn.normalize(text)
+
+# #URLの除去
+# text_without_url = re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', '', normalized_text)
+
+# #桁区切りの除去と数字の置換
+# tmp = re.sub(r'(\d)([,.])(\d+)', r'\1\3', text_without_url)
+# text_replaced_number = re.sub(r'\d+', '', tmp)
+
+# # 半角記号の置換
+# tmp = re.sub(r'[!-/:-@[-`{-~•]', r' ', text_replaced_number)
+
+# # 全角記号の置換 (ここでは0x25A0 - 0x266Fのブロックのみを除去)
+# text_removed_symbol = re.sub(u'[■-♯]', '、', tmp)
+
+# with open("sentence_after.txt", mode='w') as f:
+#     f.write(text_removed_symbol)
+
+print(normalized_text)
