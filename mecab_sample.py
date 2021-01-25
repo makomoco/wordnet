64655887a3de4445a6ca55611552c8026b9b655e
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-25 20:00:23 +0900

    Update mecab_sample.py

--- mecab_sample.py
+++ mecab_sample.py
@@ -10,7 +10,7 @@ with open("sentence_after.txt") as f:
     text = f.read()
 
 # 日本語文章の解析処理
-#ma = m.parse(text)
+ma = m.parse(text)
 
 # 濁点処理
 text = unicodedata.normalize('NFC', text)
@@ -21,8 +21,10 @@ text = unicodedata.normalize('NFC', text)
 nouns = [line.split()[0] for line in m.parse(text).splitlines()
                if "名詞" in line.split()[-1]]
 
+# 重複削除
 nouns_s = set(nouns)
 
+# リスト化
 nouns_l = list(nouns_s)
 
 # for str in nouns_l:
