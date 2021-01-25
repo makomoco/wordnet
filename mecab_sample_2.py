424016e678a08fd2b448cb2cf2852858b7c87c72
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-25 17:26:51 +0900

    Create mecab_sample_2.py

---
+++ mecab_sample_2.py
@@ -0,0 +1,34 @@
+# MeCabモジュールのインポート
+import MeCab
+import unicodedata
+
+# MeCab::Taggerクラスのインスタンスを作成（ここではデフォルト設定）
+m = MeCab.Tagger('-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd')
+
+#sentence = '鬼滅の刃の映画を見に行った。'
+with open("sentence_after_2.txt") as f:
+    text = f.read()
+
+# 日本語文章の解析処理
+#ma = m.parse(text)
+
+# 濁点処理
+text = unicodedata.normalize('NFC', text)
+
+# nouns = [line for line in m.parse(text).splitlines()
+#                if "名詞" in line.split()[-1]]
+
+nouns = [line.split()[0] for line in m.parse(text).splitlines()
+               if "名詞" in line.split()[-1]]
+
+nouns_s = set(nouns)
+
+nouns_l = list(nouns_s)
+
+# for str in nouns_l:
+#    print(str.split())
+
+print(nouns_l)
+
+# 解析結果を表示
+#print(ma)
