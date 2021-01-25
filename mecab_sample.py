b290ad95f3faf0cc1419ad7d21084d1cd3e4f096
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-25 20:04:17 +0900

    Update mecab_sample.py

--- mecab_sample.py
+++ mecab_sample.py
@@ -5,9 +5,9 @@ import unicodedata
 # MeCab::Taggerクラスのインスタンスを作成（ここではデフォルト設定）
 m = MeCab.Tagger('-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd')
 
-#sentence = '鬼滅の刃の映画を見に行った。'
-with open("sentence_after.txt") as f:
-    text = f.read()
+text = '九州大学は九州の北に位置する大学である。'
+# with open("sentence_after.txt") as f:
+#     text = f.read()
 
 # 日本語文章の解析処理
 ma = m.parse(text)
