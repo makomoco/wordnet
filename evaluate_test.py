d7686579c869fcd4b45fbd13a15acd8320be612b
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-27 16:20:37 +0900

    Update evaluate_test.py

--- evaluate_test.py
+++ evaluate_test.py
@@ -51,4 +51,4 @@ x22 = X[22] # 分追加
 
 
 # コサイン類似度（正規化）
-print(evaluate.cosine_similarity_normalized(evaluate.vector_normalized(x3), evaluate.vector_normalized(x0)))
+print(evaluate.cosine_similarity_normalized(evaluate.vector_normalized(x3), evaluate.vector_normalized(x16)))
