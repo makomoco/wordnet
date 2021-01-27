b4682743095488af981ad47a59dd073058a8f39a
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-27 15:31:09 +0900

    a

--- evaluate.py
+++ evaluate.py
@@ -1,2 +1,13 @@
+import numpy as np
+
+# コサイン類似度
 def cosine_similarity(x, y):
     return np.dot(x, y) / (np.sqrt(np.dot(x, x)) * np.sqrt(np.dot(y, y)))
+
+# コサイン類似度（正規化）
+def cosine_similarity_normalized(x, y):
+    return np.dot(x, y)
+
+# ベクトル正規化
+def vector_normalized(x):
+    return x / np.linalg.norm(x)
---
+++ evaluate_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import evaluate
+
+X = np.array([1,2],[-1,2],[2,4])
+
+x0 = X[0]
+x1 = X[1]
+x2 = X[2]
+
+print(cosine_similarity_normalized(vector_normalized(x0), vector_normalized(x1)))
