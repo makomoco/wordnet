23c86b29c3866fc3282c9151b6b1463cb28e29a7
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-28 15:35:45 +0900

    Update vec_size5.py

--- vec_size5.py
+++ vec_size5.py
@@ -86,7 +86,7 @@ l3_l2_and_list = list(l3_l2_and)
 
 ############################
 # 複合語の５次元平均ベクトルを求める
-aa_1 = model.kv['節約']
+aa_1 = model.kv['ミルク']
 aa_2 = model.kv['モード']
 # aa_3 = model.kv['テーブル']
 # aa_4 = model.kv['方式']
