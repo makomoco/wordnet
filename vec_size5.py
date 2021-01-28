f42316775c4c3fb45df6e1fdca713cc98a14a39e
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-28 15:24:15 +0900

    Update vec_size5.py

--- vec_size5.py
+++ vec_size5.py
@@ -110,7 +110,7 @@ print(avg)
 # np配列化
 avg_np = np.array(avg)
 print(avg_np)
-距離計算
+# 距離計算
 distance = np.linalg.norm(avg_np) # l3_l2_and_list内の全単語について原点からの距離を計算（ユークリッド距離）
                                     # ユークリッド距離で計算して良い（https://ja.wikipedia.org/wiki/ポワンカレの円板モデル　参照）
 d_p = 1 + 2 * ((distance**2) / (1 - (distance**2))) #arccoshの中身
