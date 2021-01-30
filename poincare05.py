e41abab09842a8664d239b38b09c3d7874b46ed1
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-01-31 00:20:51 +0900

    Update poincare05.py

--- poincare05.py
+++ poincare05.py
@@ -28,4 +28,4 @@ example_words = ['沸騰','高温','水位']
 
 # プロット
 figure_title = ''
-iplot(poincare_2d_visualization(model, relations_set, figure_title, num_nodes=None, show_node_labels=example_words)
+iplot(poincare_2d_visualization(model, relations_set, figure_title, num_nodes=None, show_node_labels=example_words))
