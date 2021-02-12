182f26d54606ebf073fb1e07a7297f0f6669d470
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-02-12 14:44:28 +0900

    Update evaluate_test.py

--- evaluate_test.py
+++ evaluate_test.py
@@ -66,7 +66,7 @@ x29 = X[29] # タイマ残り時間表示
 
 
 # コサイン類似度（正規化）
-print(evaluate.cosine_similarity_normalized(evaluate.vector_normalized(x23), evaluate.vector_normalized(x0)))
+print(evaluate.cosine_similarity_normalized(evaluate.vector_normalized(x23), evaluate.vector_normalized(x2)))
 
 # # コサイン類似度結果(上位語,下位語)
 # cos_sim_l = [[0.9619423890352065,('状態表示','ロックランプ')],
@@ -86,7 +86,20 @@ print(evaluate.cosine_similarity_normalized(evaluate.vector_normalized(x23), eva
 #              [-0.9488005806720523,('状態表示','モード表示')],
 #              [-0.8526821917637258,('水位検知','水位メータ')],
 #              [-0.6465811801880053,('水位検知','水位センサ')],
-#              [0.9999539132401888,('エラー検知','高温エラー')]]
+#              [0.9999539132401888,('エラー検知','高温エラー')],
+
+#             [0.8684569675045664,('話題沸騰ポッド','状態表示')],
+#             [(話題沸騰ポッド,ポンプ)],
+#             [(話題沸騰ポッド,温度制御)],
+#             [(話題沸騰ポッド,水位検知)],
+#             [(話題沸騰ポッド,エラー検知)],
+#             [(話題沸騰ポッド,キッチンタイマ)],
+#             [(センサ,サーミスタ)],
+#             [(加熱,ヒータ)],
+#             [(キッチンタイマ,タイマ起動)],
+#             [(キッチンタイマ,分追加)],
+#             [(キッチンタイマ,タイマボタン)],
+#             [(キッチンタイマ,タイマ残り時間表示)]]
 
 # print(sorted(cos_sim_l, reverse=True))
 
