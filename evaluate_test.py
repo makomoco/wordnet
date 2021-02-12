9e1a30ddb7dc13c0d6678ca4763479ae72e3d3dc
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-02-12 14:49:32 +0900

    Update evaluate_test.py

--- evaluate_test.py
+++ evaluate_test.py
@@ -66,7 +66,7 @@ x29 = X[29] # タイマ残り時間表示
 
 
 # コサイン類似度（正規化）
-print(evaluate.cosine_similarity_normalized(evaluate.vector_normalized(x26), evaluate.vector_normalized(x22)))
+print(evaluate.cosine_similarity_normalized(evaluate.vector_normalized(x26), evaluate.vector_normalized(x28)))
 
 # # コサイン類似度結果(上位語,下位語)
 # cos_sim_l = [[0.9619423890352065,('状態表示','ロックランプ')],
@@ -97,7 +97,7 @@ print(evaluate.cosine_similarity_normalized(evaluate.vector_normalized(x26), eva
 #             [-0.7020955359832202,(センサ,サーミスタ)],
 #             [-0.72050828179146,(加熱,ヒータ)],
 #             [0.9549913159713612,(キッチンタイマ,タイマ起動)],
-#             [(キッチンタイマ,分追加)],
+#             [-0.9999990460765702,(キッチンタイマ,分追加)],
 #             [(キッチンタイマ,タイマボタン)],
 #             [(キッチンタイマ,タイマ残り時間表示)]]
 
