cf93028722074af5042d3daaee0fb2348b7c855b
makomoco <yamada.makoto.210@s.kyushu-u.ac.jp>
2021-02-12 14:20:04 +0900

    a

--- poincare03_maceb_ver.py
+++ poincare03_maceb_ver.py
@@ -88,23 +88,23 @@ ls = [l for l in list_from_POL if l in list_from_WordNet_2]
 
 ##########################################
 # 複合語の平均ベクトルを求める
-aa_1 = model.kv['タイマー']
-aa_2 = model.kv['残り']
-aa_3 = model.kv['時間']
-aa_4 = model.kv['表示']
+aa_1 = model.kv['話題']
+aa_2 = model.kv['沸騰']
+aa_3 = model.kv['ポッド']
+# aa_4 = model.kv['表示']
 
 bb_1 = aa_1.tolist()
 bb_2 = aa_2.tolist()
 bb_3 = aa_3.tolist()
-bb_4 = aa_4.tolist()
+# bb_4 = aa_4.tolist()
 
 # print(bb_1)
 # print(bb_2)
 # print(bb_3)
 
 
-avg_x = (bb_1[0] + bb_2[0] + bb_3[0] + bb_4[0]) / 4
-avg_y = (bb_1[1] + bb_2[1] + bb_3[1] + bb_4[1]) / 4
+avg_x = (bb_1[0] + bb_2[0] + bb_3[0] ) / 3
+avg_y = (bb_1[1] + bb_2[1] + bb_3[1] ) / 3
 
 avg = [avg_x,avg_y]
 
